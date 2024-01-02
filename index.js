f358c62e69d2d9da467c8dcb4ce74d72a95cde6b
steffi w <steffiwidjaya01@gmail.com>
2024-01-02 23:37:54 +0700

    edit scatter plot

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
  
-const port = 370;
+const port = 320;
 app.set("view engine", "ejs");
 app.use(express.static("Assets"));
 app.use(express.static("public"));
